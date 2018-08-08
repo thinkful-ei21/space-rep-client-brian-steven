6bb14f67fb4f5637c1b8c0f4cddc1f685b239674
Steven Imus <imussg@gmail.com>
2018-08-08 16:19:48 -0400

    save

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,4 +1,8 @@
 import {API_BASE_URL} from '../config';
+import questions from '../questions-list-json';
+console.log(...questions);
+let count = 0;
+
 export const FETCH_QUESTIONS_REQUEST = 'FETCH_QUESTIONS_REQUEST';
 export const fetchQuestionsRequest = () => ({
   type: FETCH_QUESTIONS_REQUEST
@@ -7,7 +11,7 @@ export const fetchQuestionsRequest = () => ({
 export const FETCH_QUESTIONS_SUCCESS = 'FETCH_QUESTIONS_SUCCESS';
 export const fetchQuestionsSuccess = questions => ({
   type: FETCH_QUESTIONS_SUCCESS,
-  questions
+  questions[count++]
 });
 
 export const FETCH_QUESTIONS_ERROR = 'FETCH_QUESTIONS_ERROR';
@@ -17,39 +21,47 @@ export const fetchQuestionsError = error => ({
 });
 
 ///need BASE URL IMPORT
-export const fetchQuestions = () => dispatch => {
-  dispatch(fetchQuestionsRequest());
-  const questions = require('../questions');
-  return questions;
-  // return fetch(`${API_BASE_URL}/questions/`).then(res => {
-  //   if(!res.ok){
-  //     return Promise.reject(res.statusText)
-  //   }
-  //   return res.json();
-  }).then(questions => dispatch(fetchQuestionsSuccess(questions)))
-  }).catch(error => {
-    console.log(error);
-    dispatch(fetchQuestionsError(error));
-  });
-};
+export const fetchQuestions = () => ({
+  type: FETCH_QUESTIONS_SUCCESS,
+  questions[count++]
+});
+// export const fetchQuestions = () => dispatch => {
+//   dispatch(fetchQuestionsRequest());
+//   return fetch(`${API_BASE_URL}/questions/`).then(res => {
+//     if(!res.ok){
+//       return Promise.reject(res.statusText)
+//     }
+//     return res.json();
+//   }).then(questions => {
+//     // console.log(questions);
+//     dispatch(fetchQuestionsSuccess(questions));
+//   }).catch(error => {
+//     console.log(error);
+//     dispatch(fetchQuestionsError(error));
+//   });
+// };
 
-export const postAnswer = (answer) => dispatch => {
-  dispatch(postAnswerRequest());
-  return fetch(`${API_BASE_URL}/questions/${answer.id}`, {
-    method: 'PUT',
-    headers: {
-      "Content-Types": "application/json"
-    },
-    body: answer
-  })
-  .then(res => {
-    if(!res.ok) {
-      return Promise.reject(res.statusText);
-    }
-    return res.json();
-  })
-  .then(questions => dispatch(fetchQuestionsSuccess(questions)))
-  .catch(error => {
-    dispatch(fetchQuestionsError(error));
-  });
-};
+export const postAnswer = (answer) => ({
+  type: FETCH_QUESTIONS_SUCCESS,
+  questions[count++]
+});
+// export const postAnswer = (answer) => dispatch => {
+//   dispatch(fetchQuestionsRequest());
+//   return fetch(`${API_BASE_URL}/questions/${answer.id}`, {
+//     method: 'PUT',
+//     headers: {
+//       "Content-Types": "application/json"
+//     },
+//     body: answer
+//   })
+//   .then(res => {
+//     if(!res.ok) {
+//       return Promise.reject(res.statusText);
+//     }
+//     return res.json();
+//   })
+//   .then(questions => dispatch(fetchQuestionsSuccess(questions)))
+//   .catch(error => {
+//     dispatch(fetchQuestionsError(error));
+//   });
+// };
